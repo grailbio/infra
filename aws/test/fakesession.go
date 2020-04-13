8610bbcf04f2ea53837743c0d6a964b61d1f079a
Prasad Gopal <pgopal@grailbio.com>
2020-04-13 10:19:46 -0700

    reflow/localcluster: use localcluster for local mode execution
    Summary:
    Implement a local cluster using the local docker client. Move reflow local
    to use the localcluster.
    
    Test Plan: Ran a local test. Have to run this in production worker1 and make sure this works ok.
    
    Reviewers: smahadevan
    
    Reviewed By: smahadevan
    
    Subscribers: pboyapalli
    
    Maniphest Tasks: T30168
    
    Differential Revision: https://phabricator.grailbio.com/D46586
    
    fbshipit-source-id: b89e95e

--- aws/test/fakesession.go
+++ aws/test/fakesession.go
@@ -1,15 +1,27 @@
 package test
 
 import (
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/grailbio/infra"
 )
 
 func init() {
 	infra.Register("fakesession", new(Session))
+	infra.Register("fakecreds", new(Creds))
 }
 
 // Session is fake session provider and should be only used in tests.
 type Session struct {
 	*session.Session
+}
+
+// Creds are nil credentials (used for testing).
+type Creds struct {
+	*credentials.Credentials
+}
+
+// Init implements infra.Provider.
+func (*Creds) Init() error {
+	return nil
 }
